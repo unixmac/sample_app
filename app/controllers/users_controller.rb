a1329d041fb27e1f7a73bc8e84d49ee01b01c7f6
Sam Bauer <unixmac@mac.com>
2010-10-11 17:20:22 -0300

    Pages for following/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
-class UsersController < ApplicationController   
-  
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+class UsersController < ApplicationController
+  before_filter :authenticate, :except => [:show, :new, :create]
   before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user, :only => :destroy
-       
+  before_filter :admin_user,   :only => :destroy
+  
   def index
-    @users = User.paginate(:page  => params[:page])
+    @users = User.paginate(:page => params[:page])
     @title = "All users"
   end
   
@@ -14,9 +13,23 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
   
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
   def new
-    @user = User.new
+    @user  = User.new
     @title = "Sign up"
   end
   
@@ -49,22 +62,21 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
-      @user.destroy
-      redirect_to users_path, :flash => { :success => "User destroyed." }
+    @user.destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
   end
-  
+
   private
 
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-
+    
     def admin_user
       @user = User.find(params[:id])
       redirect_to(root_path) if !current_user.admin? || current_user?(@user)
     end
-      
 end
